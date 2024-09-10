ada62f4227b7b576fc6c059a8bf4c9b4886a6c2f
Duy Ho <duyh@mindarc.com.au>
2024-09-10 04:39:21 +0000

    Income_and_expense

---
+++ app/Models/Expense.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model 
+{
+    protected $table = 'expenses';
+
+    protected $fillable = ['user_id' , 'category_id', 'wallet_id', 'amount', 'date' , 'description'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function wallet(){
+        return $this->belongsTo(Wallet::class);
+    }
+}
